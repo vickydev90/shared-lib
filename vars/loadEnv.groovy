182bf5bc2cd8e8ec242cf9c42b0eedcff8b41d2a
vishal deshmukh <vishaldeshmukh@vishals-macbook-air.home>
2020-06-27 16:24:14 +0100

    up

--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -15,7 +15,7 @@ def call() {
   //String configPath = config.envFile ? config.envFile : "${env.WORKSPACE}/staging.properties"
   String configPath = "${env.WORKSPACE}/staging.properties"
   //configFile = readFile configPath
-  writeFile file: 'stage.properties', text: readFile(configPath)
+  writeFile file: 'stage.properties', text: readFile(file: configPath)
 
   properties = new Properties()
   File propertiesFile = new File(stage.properties)
