6f916f2aada468f5f48f4fd40b23285c17b19b6d
vishal deshmukh <vishaldeshmukh@vishals-macbook-air.home>
2020-06-27 15:18:33 +0100

    up

--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -9,30 +9,30 @@
     String value =(String) properties.getProperty(key)
     env."${key}" = "${value}"
     }
-}*/
+}
 
 def call(Map config) {
   String configPath = config.envFile ? config.envFile : "${env.WORKSPACE}/staging.properties"
   Map configFile = readFile configPath
 
   properties = new Properties()
-  //File propertiesFile = new File(configFile)
-  properties.load(configFile.newDataInputStream())
+  File propertiesFile = new File(configFile)
+  properties.load(propertiesFile.newDataInputStream())
   Set<Object> keys = properties.keySet();
   for(Object k:keys){
   String key = (String)k;
   String value =(String) properties.getProperty(key)
   env."${key}" = "${value}"
   }
-}
+}*/
 
-/*def call() {
-    def filename = readProperties  file: "${env.WORKSPACE}/staging.properties"
+def call() {
+    def filename = "${env.WORKSPACE}/staging.properties"
     echo "DEBUG: loading filename: $filename"
-    env_string = libraryResource filename
+    env_string = readFile filename
     echo "DEBUG: properties for build:\n$env_string"
 
     Properties props = new Properties()
     props.load(new ByteArrayInputStream(env_string.getBytes()))
     return props
-}*/
+}
