144a1dc99a16dfb279c365134b79df01473ed048
vishal deshmukh <vishaldeshmukh@vishals-macbook-air.home>
2020-06-29 18:44:38 +0100

    updates

--- vars/bazelWorkflow.groovy
+++ vars/bazelWorkflow.groovy
@@ -5,10 +5,30 @@ def call(Map args) {
   
   def label = "jen-agent-${UUID.randomUUID().toString()}"
   def config = args.envFile
+  boolean checkoutSubmodule = args.checkoutSubmodule
+
   podTemplate(label: label, containers: containers) {
     node(label) {
         stage('Checkout') {
-          checkout scm
+          if(checkoutSubmodule == true) {
+            checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/master'']], 
+            doGenerateSubmoduleConfigurations: false, 
+            extensions: [[
+              $class: 'SubmoduleOption', 
+              disableSubmodules: false, 
+              parentCredentials: true, 
+              recursiveSubmodules: true, 
+              reference: '', 
+              trackingSubmodules: false
+            ]], 
+            submoduleCfg: [], 
+            userRemoteConfigs: [[credentialsId: 'git-cred', url:'https://github.com/vickydev90/bazel-example-cpp.git']]])
+          }
+          else {
+            checkout scm
+          }
         }
         stage('setting env') {
             loadEnv(config)
