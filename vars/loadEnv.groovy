ab41b47d9a0c94b48847f878c8d17d76ea9ae368
vishal deshmukh <vishaldeshmukh@vishals-macbook-air.home>
2020-06-29 15:50:00 +0100

    up

--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -1,5 +1,5 @@
 def call(config){
-    String configPath = config ? config : "${env.WORKSPACE}/staging.properties"
+    String configPath = "${env.WORKSPACE}/config" ? "${env.WORKSPACE}/config" : "${env.WORKSPACE}/staging.properties"
     def props = readProperties  file: configPath
     keys= props.keySet()
     for(key in keys) {
