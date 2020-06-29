8964348bde04bdcf382dd0ef86ba0f758ba358f4
vishal deshmukh <vishaldeshmukh@vishals-macbook-air.home>
2020-06-29 15:39:33 +0100

    up

---
+++ vars/bazelWorkflow.groovy
@@ -0,0 +1,18 @@
+def call(Map args) {
+  def containers = [
+    containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave', ttyEnabled: true)
+  ]
+  
+  def label = "jen-agent-${UUID.randomUUID().toString()}"
+  def config = args.envFile
+  podTemplate(label: label, containers: containers) {
+    node(label) {
+        stage('Checkout') {
+          checkout scm
+        }
+        stage('setting env') {
+            loadEnv(config)
+        }  
+        stage('Install') {
+              sh "${BAZEL_TOOLS}"
+        }
--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -1,19 +1,5 @@
-/*def call() {
-    //def path = readProperties file: "${env.WORKSPACE}/staging.properties"
-    writeFile file: 'stage.properties', text: readFile("${env.WORKSPACE}/staging.properties")
-    properties = new Properties()
-    File propertiesFile = new File("stage.properties")
-    properties.load(propertiesFile.newDataInputStream())
-    Set<Object> keys = properties.keySet();
-    for(Object k:keys){
-      String key = (String)k;
-      String value =(String) properties.getProperty(key)
-      env."${key}" = "${value}"
-    }
-}*/
-
-def call(Map config){
-    String configPath = config.envFile ? config.envFile : "${env.WORKSPACE}/staging.properties"
+def call(config){
+    String configPath = config ? config : "${env.WORKSPACE}/staging.properties"
     def props = readProperties  file: configPath
     keys= props.keySet()
     for(key in keys) {
@@ -21,31 +7,3 @@ def call(Map config){
         env."${key}" = "${value}"
     }
 }
-
-/*def call() {
-  //String configPath = config.envFile ? config.envFile : "${env.WORKSPACE}/staging.properties"
-  configPath = libraryResource "dev/stage.properties"
-  //configFile = readFile configPath
-  //writeFile file: 'stage.properties', text: configPath
-
-  properties = new Properties()
-  //File propertiesFile = new File(configPath)
-  properties.load(configPath.newDataInputStream())
-  Set<Object> keys = properties.keySet();
-  for(Object k:keys){
-    String key = (String)k;
-    String value =(String) properties.getProperty(key)
-    env."${key}" = "${value}"
-  }
-}*/
-
-/*def call() {
-    def filename = "${env.WORKSPACE}/staging.properties"
-    echo "DEBUG: loading filename: $filename"
-    env_string = readFile filename
-    //echo "DEBUG: properties for build:\n$env_string"
-
-    Properties props = new Properties()
-    props.load(new ByteArrayInputStream(env_string.getBytes()))
-    return props
-}*/
