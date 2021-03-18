0a7d4574a46e9a4ede387b5ed8f4e5457618f604
vickydev90 <vishme00786@gmail.com>
2021-03-18 16:51:48 +0000

    Update loadEnv.groovy

--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -1,8 +1,11 @@
 import org.lab.*
     
-def call(config){
+def call(config, fname){
     String configPath = config ? config : "${env.WORKSPACE}/staging.properties"
+    String folderName = fname
     
     def utils = new envbuild()
     utils.envbuild(configPath)
+    
+    utils.roleAssume(folderName)
 }
