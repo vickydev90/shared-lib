8819ef7110426051dfc235cb38f4c8b94f3934e0
vickydev90 <vishme00786@gmail.com>
2021-03-18 15:45:22 +0000

    Update loadEnv.groovy

--- vars/loadEnv.groovy
+++ vars/loadEnv.groovy
@@ -1,9 +1,6 @@
 def call(config){
     String configPath = config ? config : "${env.WORKSPACE}/staging.properties"
-    def props = readProperties  file: configPath
-    keys= props.keySet()
-    for(key in keys) {
-        value = props["${key}"]
-        env."${key}" = "${value}"
-    }
+    
+    def utils = new envbuild(this)
+    utils.envbuild(configPath)
 }
