2dde11f3c83bdaa6b487a6066d45f5cef7105e86
vickydev90 <vishme00786@gmail.com>
2021-03-18 15:40:25 +0000

    Update envbuild.groovy

--- src/org/lab/envbuild.groovy
+++ src/org/lab/envbuild.groovy
@@ -1,1 +1,10 @@
+package org.lab
 
+def envbuild(config) {
+  def props = readProperties  file: configPath
+  keys= props.keySet()
+  for(key in keys) {
+      value = props["${key}"]
+      env."${key}" = "${value}"
+  }
+}
